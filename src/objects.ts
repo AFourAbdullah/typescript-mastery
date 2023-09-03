e660bcdb6d5a0dc26e226c9aa84d4bc1fd4d13d9
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-03 23:15:15 +0500

    code added

--- src/objects.ts
+++ src/objects.ts
@@ -13,3 +13,36 @@ function createUser(user: Users) {
   console.log(user);
 }
 createUser({ name: "ABdullah", isPaid: true, email: "abc@gmail.com" });
+
+function createProduct(product: {
+  name: string;
+  stock: number;
+  isAvailable: boolean;
+}) {
+  console.log(product);
+}
+createProduct({ name: "Product1", isAvailable: true, stock: 300 });
+const productMker: createProductFunc = function createProduct(product) {
+  console.log(product);
+};
+console.log(productMker({ name: "p1", isAvailable: false, stock: 33 }));
+
+type createProductFunc = (product: {
+  name: string;
+  stock: number;
+  isAvailable: boolean;
+}) => void;
+
+interface Course {
+  name: string;
+  price: number;
+}
+
+type createCourseFunc = (course: Course) => Course;
+
+const createCourse: createCourseFunc = (course) => {
+  console.log(course);
+  return course;
+};
+
+createCourse({ name: "ts basics", price: 200 });
